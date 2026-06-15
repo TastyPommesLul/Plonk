50d8fff3d01a715df5cb850ee680459c0f898640
TastyPommesLul <110435494+TastyPommesLul@users.noreply.github.com>
2026-06-15 10:17:53 +0200

    forget about that, i just found out its a thing in the SimpleCookingRecipeBuilder class' smelting method

--- neoforge/src/main/java/dev/tastypommeslul/plonk/datagen/PlonkRecipeProvider.java
+++ neoforge/src/main/java/dev/tastypommeslul/plonk/datagen/PlonkRecipeProvider.java
@@ -2,16 +2,8 @@ package dev.tastypommeslul.plonk.datagen;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
-import net.minecraft.world.item.ItemStackTemplate;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.CookingBookCategory;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.SmeltingRecipe;
-import net.minecraft.world.level.ItemLike;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,22 +15,6 @@ public class PlonkRecipeProvider extends RecipeProvider {
     @Override
     protected void buildRecipes() {
 
-    }
-
-    private void multiSmelt(ItemLike input, ItemLike result, RecipeCategory recipeCategory,
-                            CookingBookCategory cookingBookCategory, int exp, int cookingTime, SmeltingRecipeFactory factory) {
-        SimpleCookingRecipeBuilder.generic(Ingredient.of(input), recipeCategory, cookingBookCategory,
-                        result, exp, cookingTime,
-                        (commonInfo, cookingBookInfo, ingredient, itemStackTemplate, v, i) -> new SmeltingRecipe(
-                                commonInfo, cookingBookInfo, ingredient, factory.create(itemStackTemplate), v, i
-                        ))
-                .unlockedBy(getHasName(Items.ITEM_FRAME), has(Items.ITEM_FRAME))
-                .save(output);
-    }
-
-    @FunctionalInterface
-    interface SmeltingRecipeFactory {
-        ItemStackTemplate create(ItemStackTemplate template);
     }
 
     public static class Runner extends RecipeProvider.Runner {
